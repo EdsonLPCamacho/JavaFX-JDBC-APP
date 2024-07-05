0ca546422ebcea3721c5116295956664ae6eae4d
Edson Camacho <edsonlpcamacho@gmail.com>
2024-07-05 08:48:59 -0300

    MainViewController Implemented

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
 module Javafxjdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql; 
+
+    opens gui to javafx.fxml; 
+    opens application to javafx.graphics; 
+
+    exports gui; 
 }
